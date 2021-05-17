b33259ac6d623c0382a0fe7b163dc6466a893e70
KooshaG <75464454+KooshaG@users.noreply.github.com>
2021-05-17 18:21:13 -0400

    made some functions

--- src/Assignment.java
+++ src/Assignment.java
@@ -13,18 +13,18 @@ public class Assignment {
         
     }
     public static int rearrangeParticipants(String[] pName, DOB[] pDOB, int n){
-        if(n==0) return 0;
+        if(n==0) return 0; //base case
         displayResult(pName, pDOB);
         for(int i=0; i<n; i++) {
-            if(pDOB[n-1].isYounger(pDOB[i]) && !pDOB[i].isSenior()) {
-                DOB temp = new DOB(pDOB[i]);
+            if(pDOB[n-1].isYounger(pDOB[i]) && !pDOB[i].isSenior()) { //if we find a non senior in the loop and the person at n is younger, 
+                DOB temp = new DOB(pDOB[i]);                          //swap because we want oldest non senior at the bottom
                 String tmp = pName[i];
                 pDOB[i]=pDOB[n-1];
                 pName[i]=pName[n-1];
                 pDOB[n-1]=temp;
                 pName[n-1]=tmp;
             }
-            else if(pDOB[i].isYounger(pDOB[n-1]) && pDOB[n-1].isSenior()){
+            else if(pDOB[i].isYounger(pDOB[n-1]) && pDOB[n-1].isSenior()){ //we want the exact opposite if the person at n is a senior
                 DOB temp = new DOB(pDOB[i]);
                 String tmp = pName[i];
                 pDOB[i]=pDOB[n-1];
@@ -38,10 +38,33 @@ public class Assignment {
         }
         return (rearrangeParticipants(pName, pDOB, n-1));
     }
+
+    public static void displaySeniorsIncreasingOrder(String[] pName, DOB[] pDOB, int seniors){
+        if(seniors==0) return;
+        System.out.println(pName[seniors-1]+"\t"+pDOB[seniors-1]);
+        displaySeniorsIncreasingOrder(pName, pDOB,seniors-1);
+    }
+
+    public static void displayNonSeniorsIncreasingOrder(String[] pName, DOB[] pDOB, int nonSeniors, int n){
+        if(n==nonSeniors) return;
+        displayNonSeniorsIncreasingOrder(pName, pDOB, nonSeniors, n-1);
+        System.out.println(pName[n-1]+"\t"+pDOB[n-1]);
+    }
+
+    public static void displayIncreasingOrder(String[] pName, DOB[] pDOB, int seniors, int n){
+
+    }
     public static void displayResult(String[] pName, DOB[] pDOB) {
         for(int i=0; i<pName.length; i++) {
             System.out.println(i+"\t"+pName[i]+"\t"+pDOB[i]);
         }
         System.out.println();
+    }
+    public static String[] nameGenerator(int n){
+        String[] names = new String[n];
+        for(int i=0; i<n; i++){
+            names[i]=Integer.toString(i);
+        }
+        return names;
     }
 }
